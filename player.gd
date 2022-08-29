bb69dabb06c225330eaccb42184b16f24299ac22
stoodleyc <68675314+stoodleyc@users.noreply.github.com>
2022-08-30 10:56:01 +1200

    movement

--- player.gd
+++ player.gd
@@ -12,14 +12,30 @@ func _ready():
 
 #variables for movement
 var velocity = Vector2.ZERO
-export (int) var fast = 5
-export (int) var speed = 2.5
+export (int) var walk = 2.5
 
 func _physics_process(delta):
+	
+	#vertical movement
 	if Input.is_action_pressed("move up"):
 		print("up")
+		velocity.y = -walk
+	elif Input.is_action_pressed("move down"):
+		print("down")
+		velocity.y = walk
+	else:
+		velocity.y = 0
 	
+	
+	#horisontal movement
 	if Input.is_action_pressed("move left"):
 		print("left")
+		velocity.x = -walk
+	elif Input.is_action_pressed("move right"):
+		print("right")
+		velocity.x = walk
+	else:
+		velocity.x = 0
 	
 	
+	move_and_collide(velocity)
