a4b3d97c68e29980d83251195a6625684624a10c
stoodleyc <68675314+stoodleyc@users.noreply.github.com>
2022-08-31 15:12:41 +1200

    pause

---
+++ pause.gd
@@ -0,0 +1,23 @@
+extends PopupPanel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	if Input.is_action_just_released("pause"):
+		if visible == true:
+			visible = false
+		else:
+			visible = true
+
+
+func _on_resume_pressed():
+	visible = false
