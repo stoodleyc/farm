adad1072230e9137a111b4c4fa35bfc245ca3851
stoodleyc <68675314+stoodleyc@users.noreply.github.com>
2022-09-19 21:53:35 +1200

    save

--- farm.gd
+++ farm.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 onready var player = $player
-onready var label = $Label
 
 onready var game_data = SaveFiles.game_data
 
@@ -9,7 +8,7 @@ func _ready():
 	SaveFiles.load_data()
 	SaveFiles.save_data()
 	if SaveFiles.enter == true:
-		label.text = game_data.player_name
+		player.position.x = game_data.player_x
 		SaveFiles.enter = false
 	else:
 		player.position = Vector2 (970,300)
